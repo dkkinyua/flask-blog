85c5fa8526746dd6288c0d8e4c87f08841816946
Denzel Kanyeki <denzelkinyua11@gmail.com>
2024-01-27 14:21:01 +0300

    Updates and tweaks

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,22 +1,15 @@
 # This file is a file to initialize our web app, contains initialization code
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import datetime as datetime
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-import smtplib
+from blog.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = b'ea8d0505b73e07a44a3bbd4a24392a45' 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
